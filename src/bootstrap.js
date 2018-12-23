c301ff764a0978026936e586ec41b2ce2a7af71a
Adrian Cozac <adrian.cozac@itiviti.com>
2018-12-23 11:00:11 +0200

    Add routes and boostrap files

---
+++ src/bootstrap.js
@@ -0,0 +1,26 @@
+import bodyParser from 'body-parser';
+import {log} from './utils';
+import routes from './routes';
+
+export default function (app) {
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true}));
+
+    app.use(routes);
+
+    app.use((req, res) => {
+        res.status(404).send({
+            status: 404,
+            message: 'The request resource was not found.',
+        });
+    });
+
+    app.use((err, req, res) => {
+        log.error(err.stack);
+        const message = process.env.NODE_ENV === 'production' ? 'Something went wrong ...' : err.stack;
+        res.status(500).send({
+            status: 500,
+            message,
+        });
+    });
+}
---
+++ src/route.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import { log } from './utils'
+
+const router = new express.Router();
+
+router.post('/slack/command/hello_Rbot', async (req, res) => {
+    try {
+        const slackReqObj = req.body;
+        const response = {
+            response_type: 'in_channel',
+            channel: slackReqObj.channel_id,
+            text: 'Hello Rbot. How can I help you ? :slightly_smiling_fase:'
+        };
+
+        return res.json(response);
+    }
+    catch (err) {
+        log.error(err);
+    }
+});
+
+export default router;
